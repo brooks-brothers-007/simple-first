be6d2f01c7ea92213c894b47a5f6776e2b06db2a
brooks-brothers-007 <75778464+brooks-brothers-007@users.noreply.github.com>
2021-04-07 22:15:35 +0900

    Revert "トップページ作成"

--- app/controllers/simple_first_controller.rb
+++ app/controllers/simple_first_controller.rb
@@ -1,10 +1,6 @@
 class SimpleFirstController < ApplicationController
   def index
-    @articles = Article.order("created_at DESC")
+    render '/index' 
+    # app/views/index.html.erbなら‘/index’ renderでviewファイルの呼び出しが可能
   end 
-
-  def new
-    @article = Article.new
-  end
-
 end
