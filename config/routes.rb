ba737d322829e4f6ec02864a9c1a798016ff4505
brooks-brothers-007 <shoutanakane@gmail.com>
2021-04-07 14:10:48 +0900

    カテゴリー表示機能ファイルの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root to: 'articles#index'
+  resources :articles
   devise_for :users
-  root to: 'simple_first#index'  
+  root to: "simple_first#index"  
+  resources :simple_first do
+  end  
 end
